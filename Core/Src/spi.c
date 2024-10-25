51a3469cbd29542dae2770f585b4f6801518eec3
APBashara <alexbashara2002@gmail.com>
2024-10-25 14:59:57 -0500

    Updated SPI GPIO config

--- Core/Src/spi.c
+++ Core/Src/spi.c
@@ -15,13 +15,18 @@ void SPI2_Init() {
   SPI2->CR1 &= ~SPI_CR1_SPE; // Disable SPI
 
   GPIOB->MODER &= ~GPIO_MODER_MODE12 & ~GPIO_MODER_MODE13
-                & ~GPIO_MODER_MODE14 & ~GPIO_MODER_MODE15; // Clear PB13, PB14, and PB15
-  GPIOB->OSPEEDR |= (0x3 << GPIO_OSPEEDR_OSPEED12_Pos) | (0x3 << GPIO_OSPEEDR_OSPEED13_Pos) 
-                | (0x3 << GPIO_OSPEEDR_OSPEED14_Pos) | (0x3 << GPIO_OSPEEDR_OSPEED15_Pos); // Set PB12, PB13, PB14, and PB15 to High Speed
+                & ~GPIO_MODER_MODE14 & ~GPIO_MODER_MODE15;
   GPIOB->MODER |= (0x1 << GPIO_MODER_MODE12_Pos) | (0x2 << GPIO_MODER_MODE13_Pos) 
-                | (0x2 << GPIO_MODER_MODE14_Pos) | (0x2 << GPIO_MODER_MODE15_Pos); // Set PB12, PB13, PB14, and PB15 to AF
+                | (0x2 << GPIO_MODER_MODE14_Pos) | (0x2 << GPIO_MODER_MODE15_Pos); // Set AF and output
   GPIOB->AFR[1] |= (0x5 << GPIO_AFRH_AFSEL13_Pos) | (0x5 << GPIO_AFRH_AFSEL14_Pos) 
                 | (0x5 << GPIO_AFRH_AFSEL15_Pos); // Set PB13, PB14, and PB15 to AF5 (SPI2)
+  GPIOB->OSPEEDR |= (0x3 << GPIO_OSPEEDR_OSPEED12_Pos) | (0x3 << GPIO_OSPEEDR_OSPEED13_Pos) 
+                | (0x3 << GPIO_OSPEEDR_OSPEED14_Pos) | (0x3 << GPIO_OSPEEDR_OSPEED15_Pos); // Set to High Speed
+  GPIOB->OTYPER &= ~GPIO_OTYPER_OT12 & ~GPIO_OTYPER_OT13
+                & ~GPIO_OTYPER_OT14 & ~GPIO_OTYPER_OT15; // Set pins to Push-Pull
+  GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD12 & GPIO_PUPDR_PUPD13
+                & GPIO_PUPDR_PUPD14 & GPIO_PUPDR_PUPD15;
+  GPIOB->PUPDR |= (0x1 << GPIO_PUPDR_PUPD12) | (0x1 << GPIO_PUPDR_PUPD14); // CS and MISO Pull Up
 
   // Set Baud Rate to fPCLK/32, SSM to Software, SSI to High
   SPI2->CR1 = (0x4 << SPI_CR1_BR_Pos) | SPI_CR1_SSM | SPI_CR1_SSI; // Set Baud Rate to fPCLK/32
